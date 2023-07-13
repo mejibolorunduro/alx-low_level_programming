85425dfa13023af43fe5188d34df5a40b61bd5b2
mejibolorunduro <davidbolorunduro2004@gmail.com>
2023-07-13 09:03:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#Include < stdio.h >
+/**
+ * main - A C program that prints a line using the function puts
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
