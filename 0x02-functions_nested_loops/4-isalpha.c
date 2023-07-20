3c4b5f17f14baf2c738d62b40fb87067a39fe61a
mejibolorunduro <davidbolorunduro2004@gmail.com>
2023-07-19 19:02:59 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha/*checks for alphabetic character*/(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
